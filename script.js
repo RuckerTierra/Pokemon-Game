fa8aa89029f119ebf4884a3bbfa77a50e7406ad8
Tierra <tierrarucker@ymail.com>
2021-07-23 20:27:20 -0400

    basic file(s) setup

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Project: build a Pokemon rock, paper, scissors game */
+// Step 1: create elements
+// Step 2: assign 
+// Step 3: 
+// Step 4: 
+// Step 5: 
+
+/* ELEMENTS */ 
+charmanderEl = document.querySelector('#charmander');
+bulbasaurEl = document.querySelector('#bulbasaur');
+squirtleEl = document.querySelector('#squirtle');
+
+/* FUNCTIONS */ 
+charmanderEl.onclick = function(event){
+  console.log("charmander clicked")
+}
+bulbasaurEl.onclick = function(event){
+  console.log("bulbasaur clicked")
+}
+squirtleEl.onclick = function(event){
+  console.log("squirtle clicked")
+}
