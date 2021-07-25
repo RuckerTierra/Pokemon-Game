d862f79d34c8578af31ec7300d87f3251995cffe
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2021-07-24 22:49:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 /* Project: build a Pokemon rock, paper, scissors game */
-// Step 1: create elements
-// Step 2: assign 
+// Step 1: create elements & assign value
+// Step 2: create function to loop through pokemons on click
 // Step 3: 
 // Step 4: 
 // Step 5: 
 
-/* ELEMENTS */ 
-charmanderEl = document.querySelector('#charmander');
-bulbasaurEl = document.querySelector('#bulbasaur');
-squirtleEl = document.querySelector('#squirtle');
 
-/* FUNCTIONS */ 
-charmanderEl.onclick = function(event){
-  console.log("charmander clicked")
+/* ELEMENTS */ 
+// var pokemonsEl = document.querySelector('.select-screen').querySelectorAll('.character');
+var pokemonsEl = document.querySelectorAll('.character');
+var gameState = {
+  userPokemon: "",
+  rivalPokemon: ""
+};
+
+
+/* FUNCTIONS */
+// console.log(pokemonsEl);
+var i = 0;
+while (i < pokemonsEl.length){
+pokemonsEl[i].onclick = function(){
+  var pokemonName = this.dataset.pokemon;
+  console.log(`i pressed this pokemon ` + pokemonName);
+  gameState.pokemon = pokemonName;
+
+  cpuPick();
+  console.log(gameState);
+  }
+  i++;
 }
-bulbasaurEl.onclick = function(event){
-  console.log("bulbasaur clicked")
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-squirtleEl.onclick = function(event){
-  console.log("squirtle clicked")
+
+function cpuPick(){
+  pokemonsEl[randomNumber(0, 3)].dataset.pokemon;
+  gameState.rivalPokemon = pokemonsEl[randomNumber(0, 3)].dataset.pokemon;
 }
